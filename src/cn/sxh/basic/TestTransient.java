7d465ee059f2b455f6fd5c845dcdea8d2506ba2c
SoDreamX <33822785+SoDreamX@users.noreply.github.com>
2017-12-01 15:40:15 +0800

    2017-12-01

--- src/cn/sxh/basic/TestTransient.java
+++ src/cn/sxh/basic/TestTransient.java
@@ -1,9 +1,47 @@
 package cn.sxh.basic;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import cn.sxh.basic.assist.User;
+
 /**
  * 使用transient关键字不序列化变量
  * @author sxh
  * 2017年11月30日 下午5:17:01
+ * 1）一旦变量被transient修饰，变量将不再是对象持久化的一部分，该变量内容在序列化后无法获得访问。
+ * 2）transient关键字只能修饰变量，而不能修饰方法和类。注意，本地变量是不能被transient关键字修饰的。变量如果是用户自定义类变量，则该类需要实现Serializable接口。
+ * 3）被transient关键字修饰的变量不再能被序列化，一个静态变量不管是否被transient修饰，均不能被序列化。
  */
 public class TestTransient {
-	
+	public static void test(){
+		User user = new User();
+		user.setUserName("username");
+		user.setPassword("password");
+		
+		//序列化开始
+		System.out.println("序列化之前:"+"用户名-"+user.getUserName()+";密码-"+user.getPassword());
+		try {
+			ObjectOutputStream ops = new ObjectOutputStream(new FileOutputStream("E:/user.txt"));
+			ops.writeObject(user);
+			ops.flush();
+			ops.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//序列化完毕
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream("E:/user.txt"));
+			user = (User)ois.readObject();
+			ois.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("序列化对象后："+"用户名-"+user.getUserName()+";密码-"+user.getPassword());
+	}
+	public static void main(String[] args) {
+		test();
+	}
 }
--- src/cn/sxh/basic/Testforeach.java
+++ src/cn/sxh/basic/Testforeach.java
@@ -1,5 +1,70 @@
 package cn.sxh.basic;
 
-public class Testforeach {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * foreach语句是java5的新特征之一
+ * 增强for循环
+ * @author sxh
+ * 2017年12月1日 下午3:23:03
+ */
+public class Testforeach {
+	/**
+	*foreach语句输出一维数组
+	*/
+	public void test1(){
+		//定义并初始化一个数组
+		int arr[]={2,3,1};
+		System.out.println("----1----排序前的一维数组");
+		for(int x:arr){
+			System.out.println(x);//逐个输出数组元素的值
+		}
+		//对数组排序
+		Arrays.sort(arr);
+		//利用java新特性foreach循环输出数组
+		System.out.println("----1----排序后的一维数组");
+		for(int x:arr){
+			System.out.println(x);//逐个输出数组元素的值
+		}
+	}
+	/**
+	*集合转换为一维数组
+	*/
+	public void listToArray(){
+		//创建List并添加元素
+		List<String>list=new ArrayList<String>();
+		list.add("1");
+		list.add("3");
+		list.add("4");
+		
+		//利用froeach语句输出集合元素
+		System.out.println("----2----froeach语句输出集合元素");
+		for(String x:list){
+			System.out.println(x);
+		}
+		
+		//将ArrayList转换为数组
+		Object s[]=list.toArray();
+		
+		//利用froeach语句输出集合元素
+		System.out.println("----2----froeach语句输出集合转换而来的数组元素");
+		for(Object x:s){
+			System.out.println(x.toString());//逐个输出数组元素的值
+		}
+	}
+	
+	/**
+	*foreach输出二维数组测试
+	*/
+	public void testArray2(){
+		int arr2[][]={{4,3},{1,2}};
+		System.out.println("----3----foreach输出二维数组测试");
+		for(int x[]:arr2){
+			for(int e:x){
+				System.out.println(e);//逐个输出数组元素的值
+			}
+		}
+	}
 }
