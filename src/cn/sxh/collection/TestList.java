98c6cbc2978aa7d88c7f46acc1119eaf8b3219cf
SoDreamX <33822785+SoDreamX@users.noreply.github.com>
2018-04-11 09:18:42 +0800

    2018/4/10

--- src/cn/sxh/collection/TestList.java
+++ src/cn/sxh/collection/TestList.java
@@ -1,5 +1,74 @@
 package cn.sxh.collection;
 
-public class TestList {
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+/**
+ * List:有序的列表，允许存放重复的元素
+ * ArrayList:数组实现，查询快，增删慢，轻量级(线程不安全)
+ * LinkedList:双向链表实现，查询慢，增删快(线程不安全)
+ * Vector:数组实现，重量级(线程安全，使用少)
+ * @author Sxh
+ * 2018年4月10日 下午1:18:22
+ */
+public class TestList {
+	/**
+	 * 提供快速的基于索引的成员访问
+	 */
+	public void testArrayList() {
+		ArrayList<String> list = new ArrayList<String>();
+		System.out.println("初始化大小："+list.size());
+		list.add("aaa");
+		list.add("egg");
+		list.add("hell");
+		list.add("child");
+		System.out.println("当前容量："+list.size());
+		/* 将ArrayList的大小和实际所含元素的大小设置一致 */    
+		list.trimToSize();
+		//遍历
+		for(String s:list) {
+			System.out.println(s);
+		}
+		//指定位置插入数据
+		list.add(2, "zhu");
+		//遍历
+		for(String s:list) {
+			System.out.println(s);
+		}
+		System.out.println("------------------");
+		//清空list
+		list.clear();
+		//遍历
+		for(String s:list) {
+			System.out.println(s);
+		}
+		System.out.println("------------------");
+	}
+	/**
+	 * 双向链表实现，查询慢，增删快
+	 */
+	public void testLinkedList() {
+		LinkedList<Integer> link = new LinkedList<>();
+		System.out.println(link.size());
+		//添加元素
+		link.add(222);
+		link.add(111);
+		link.add(0);
+		link.add(3333);
+		link.add(8888);
+		System.out.println(link.size());
+		//遍历
+		for(int i:link) {
+			System.out.println(i);
+		}
+		/* 获取第一个元素 ,即header的next域*/    
+        System.out.println("第一个元素是:" + link.getFirst());       
+        /*获取最后一个元素,即header的previous域*/    
+        System.out.println("最后一个元素是:"+link.getLast()); 
+	}
+	public static void main(String[] args) {
+		TestList test = new TestList();
+		test.testArrayList();
+		test.testLinkedList();
+	}
 }
--- src/cn/sxh/collection/TestMap.java
+++ src/cn/sxh/collection/TestMap.java
@@ -3,15 +3,22 @@ package cn.sxh.collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 /**
  * MAP:保存键值对成员，基于键找值操作，compareTo或compare方法对键排序
+ * HashMap:键值对，key不能重复，value可以重复；允许null的键或值
+ * Hashtable:线程安全，不允许null键或值
  * @author Sxh
  * 2018年4月9日 上午11:33:17
  */
 public class TestMap {
 	/**
 	 * 键值对，key不能重复，value可以重复；允许null的键或值
+	 * HashMap线程同步的方法：
+	 * ①.Map<Integer,String> hashMap = new HashMap<Integer,String>();
+	 * 	 hashMap = Collections.syncchronizedMap(hashMap);
+	 * ②.ConcurrentHashMap<Integer,String> map = new ConcurrentHashMap<Integer,String>();
 	 */
 	public void testHashMap() {
 		Map<String, Integer> hash = new HashMap<String, Integer>();
@@ -23,7 +30,7 @@ public class TestMap {
 		hash.put("egg", 3);
 		System.out.println(hash.size());
 		System.out.println("hashMap是否为空:"+(hash.isEmpty()?"是":"否"));
-		//遍历元素
+		//遍历元素--无序
 		Set<String> set = hash.keySet();
 		for(String s:set) {
 			System.out.println(s+"---"+hash.get(s)+",hashcode---"+s.hashCode());
@@ -32,9 +39,27 @@ public class TestMap {
 		System.out.println(hash.containsValue(2));
 		System.out.println(hash.hashCode());
 	}
+	/**
+	 * TreeMap--对key排好序的map;key就是TreeSet,value对应每个key
+	 */
+	public void testTreeMap() {
+		Map<String,Integer> map = new TreeMap<>();
+		//添加
+		map.put("zgg", 1);
+		map.put("erqing", 3);
+		map.put("niu", 0);
+		map.put("abc", 2);
+		map.put("aaa", 5);
+		//遍历--有序
+		Set<String> set = map.keySet();
+		for(String s:set) {
+			System.out.println(map.get(s)+"--"+s);
+		}
+	}
 	
 	public static void main(String[] args) {
 		TestMap test = new TestMap();
 		test.testHashMap();
+		test.testTreeMap();
 	}
 }
