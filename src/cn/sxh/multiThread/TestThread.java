4a153c37f2a38508c2116d2927ba4eac0bfaff74
SoDreamX <33822785+SoDreamX@users.noreply.github.com>
2018-06-11 17:29:14 +0800

    多线程完毕

--- src/cn/sxh/multiThread/TestThread.java
+++ src/cn/sxh/multiThread/TestThread.java
@@ -3,13 +3,19 @@ package cn.sxh.multiThread;
 import cn.sxh.collection.bean.StageThread;
 import cn.sxh.multiThread.myThread.Actor;
 import cn.sxh.multiThread.myThread.Actress;
+import cn.sxh.multiThread.myThread.MyThreadPrinter;
 
 public class TestThread {
 	
 	public static void main(String[] args) {
 		TestThread test = new TestThread();
 		//test.testActor();
-		test.testArmy();
+		//test.testArmy();
+		try {
+			test.testThreadPrinter();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void testArmy() {
@@ -26,5 +32,20 @@ public class TestThread {
 		Actress actress = new Actress();
 		Thread thd = new Thread(actress,"Ms.Runnable");
 		thd.start();
+	}
+	public void testThreadPrinter() throws InterruptedException {
+		Object a = new Object();
+		Object b = new Object();
+		Object c = new Object();
+		MyThreadPrinter printa = new MyThreadPrinter("A", c, a);
+		MyThreadPrinter printb = new MyThreadPrinter("B", a, b);
+		MyThreadPrinter printc = new MyThreadPrinter("C", b, c);
+		new Thread(printa).start();
+		Thread.sleep(10);
+		new Thread(printb).start();
+		Thread.sleep(10);
+		new Thread(printc).start();
+		Thread.sleep(10);
+		System.out.println("mainœﬂ≥ÃÕÍ±œ!");
 	}
 }
